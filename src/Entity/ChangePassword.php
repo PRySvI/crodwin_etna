f66f0e1260351c5927976ca2079657cc93f2bcd0
rusty-cage <rabhimyriam@gmail.com>
2018-12-28 18:24:41 +0100

    nouveau password

--- src/Entity/ChangePassword.php
+++ src/Entity/ChangePassword.php
@@ -3,6 +3,9 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ChangePasswordRepository")
